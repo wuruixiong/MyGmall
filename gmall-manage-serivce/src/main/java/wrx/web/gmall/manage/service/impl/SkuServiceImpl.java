022925d09b815091fb874f042699a87a068defc7
wuruixiong <wowuruixiong@163.com>
2019-12-21 16:48:18 +0800

    add redis

--- gmall-manage-serivce/src/main/java/wrx/web/gmall/manage/service/impl/SkuServiceImpl.java
+++ gmall-manage-serivce/src/main/java/wrx/web/gmall/manage/service/impl/SkuServiceImpl.java
@@ -2,7 +2,10 @@ package wrx.web.gmall.manage.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
 import wrx.web.gmall.bean.PmsSkuAttrValue;
 import wrx.web.gmall.bean.PmsSkuImage;
 import wrx.web.gmall.bean.PmsSkuInfo;
@@ -12,6 +15,7 @@ import wrx.web.gmall.manage.mapper.PmsSkuImageMapper;
 import wrx.web.gmall.manage.mapper.PmsSkuInfoMapper;
 import wrx.web.gmall.manage.mapper.PmsSkuSaleAttrValueMapper;
 import wrx.web.gmall.service.SkuService;
+import wrx.web.gmall.util.RedisUtil;
 
 import java.util.List;
 
@@ -30,6 +34,8 @@ public class SkuServiceImpl implements SkuService {
     @Autowired
     PmsSkuImageMapper pmsSkuImageMapper;
 
+    @Autowired
+    RedisUtil redisUtil;
 
     @Override
     public void saveSkuInfo(PmsSkuInfo pmsSkuInfo) {
@@ -63,15 +69,56 @@ public class SkuServiceImpl implements SkuService {
     @Override
     public PmsSkuInfo getSkuById(String skuId) {
         PmsSkuInfo pmsSkuInfo = new PmsSkuInfo();
+        // 链接缓存
+        Jedis jedis = redisUtil.getJedis();
+        // 查询redis缓存
+        String skuKey = "sku:"+skuId+":info";
+        String skuJson = jedis.get(skuKey);
+
+        if(StringUtils.isNotBlank(skuJson)){//if(skuJson!=null&&!skuJson.equals(""))
+            // 如果命中redis缓存，直接返回数据
+            pmsSkuInfo = JSON.parseObject(skuJson, PmsSkuInfo.class);
+        }else{
+            // 如果缓存中没有，查询mysql
+
+            // 设置分布式锁
+            String OK = jedis.set("sku:" + skuId + ":lock", "1", "nx", "px", 10);
+            if(StringUtils.isNotBlank(OK)&&OK.equals("OK")){
+                // 设置成功，有权在10秒的过期时间内访问数据库
+                pmsSkuInfo =  getSkuByIdFromDb(skuId);
+                if(pmsSkuInfo!=null){
+                    // mysql查询结果存入redis
+                    jedis.set("sku:"+skuId+":info", JSON.toJSONString(pmsSkuInfo));
+                }else{
+                    // 数据库中不存在该sku
+                    // 为了防止缓存穿透将，null或者空字符串值设置给redis
+                    jedis.setex("sku:"+skuId+":info",60*3,JSON.toJSONString(""));
+                }
+            }else{
+                // 设置失败，自旋（该线程在睡眠几秒后，重新尝试访问本方法）
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return getSkuById(skuId);
+            }
+        }
+        jedis.close();
+        return pmsSkuInfo;
+    }
+
+    public PmsSkuInfo getSkuByIdFromDb(String skuId){
+        // sku商品对象
+        PmsSkuInfo pmsSkuInfo = new PmsSkuInfo();
         pmsSkuInfo.setId(skuId);
         PmsSkuInfo skuInfo = pmsSkuInfoMapper.selectOne(pmsSkuInfo);
 
-        //sku图片集合
+        // sku的图片集合
         PmsSkuImage pmsSkuImage = new PmsSkuImage();
         pmsSkuImage.setSkuId(skuId);
         List<PmsSkuImage> pmsSkuImages = pmsSkuImageMapper.select(pmsSkuImage);
         skuInfo.setSkuImageList(pmsSkuImages);
-
         return skuInfo;
     }
 
