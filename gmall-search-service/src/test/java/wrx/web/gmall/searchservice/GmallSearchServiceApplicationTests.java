02f369010e791fee75a9613d45f6edfbe1690c7a
wuruixiong <wowuruixiong@163.com>
2020-01-27 00:05:00 +0800

    config test

--- gmall-search-service/src/test/java/wrx/web/gmall/searchservice/GmallSearchServiceApplicationTests.java
+++ gmall-search-service/src/test/java/wrx/web/gmall/searchservice/GmallSearchServiceApplicationTests.java
@@ -1,13 +1,23 @@
 package wrx.web.gmall.searchservice;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import io.searchbox.client.JestClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import wrx.web.gmall.service.SkuService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GmallSearchServiceApplicationTests {
+
+    @Reference
+    SkuService skuService;// 查询mysql
+
+    @Autowired
+    JestClient jestClient;
 
     @Test
     public void contextLoads() {
