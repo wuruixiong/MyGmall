d96ca7241b752a616bbbb0d38c3fcf64c2616187
wuruixiong <wowuruixiong@163.com>
2020-01-27 17:40:27 +0800

    add search service

---
+++ gmall-search-web/src/main/java/wrx/web/gmall/searchweb/controller/SearchController.java
@@ -0,0 +1,38 @@
+package wrx.web.gmall.searchweb.controller;
+
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import wrx.web.gmall.bean.PmsSearchParam;
+import wrx.web.gmall.bean.PmsSearchSkuInfo;
+import wrx.web.gmall.service.SearchService;
+import wrx.web.gmall.service.SkuService;
+
+import java.util.List;
+
+@Controller
+public class SearchController {
+
+    @Reference
+    SkuService skuService;// 查询mysql
+
+    @Reference
+    SearchService searchService;
+
+    @RequestMapping("list.html")
+    public String list(PmsSearchParam pmsSearchParam, ModelMap modelMap){// 三级分类id、关键字、
+
+        // 调用搜索服务，返回搜索结果
+        List<PmsSearchSkuInfo> pmsSearchSkuInfos =  searchService.list(pmsSearchParam);
+        modelMap.put("skuLsInfoList",pmsSearchSkuInfos);
+
+        return "list";
+    }
+
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
+}
---
+++ gmall-search-web/src/test/java/wrx/web/gmall/searchweb/GmallSearchWebApplicationTests.java
@@ -0,0 +1,13 @@
+package wrx.web.gmall.searchweb;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallSearchWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
